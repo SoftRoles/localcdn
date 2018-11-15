753640043885be5e179ceeda6eab6afe204f2fbc
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-11-15 10:58:46 +0300

    initial libraries added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 
 const app = express();
-app.use("/cdn", express.static(path.join(__dirname, "node_modules")))
+app.use("/localcdn", express.static(path.join(__dirname, "node_modules")))
+app.use("/localcdn", express.static(path.join(__dirname, "bower_components")))
 
 app.listen(3009, function () {
   console.log("Service running on http://127.0.0.1:3009")
