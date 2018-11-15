76939ecf7618fb2443e3a011db6d3b137b2d1cca
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-11-15 11:38:26 +0300

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var path = require('path');
+var cors = require("cors")
 
 const app = express();
 app.use("/localcdn", express.static(path.join(__dirname, "node_modules")))
 app.use("/localcdn", express.static(path.join(__dirname, "bower_components")))
+
+app.use(cors())
 
 app.listen(3009, function () {
   console.log("Service running on http://127.0.0.1:3009")
