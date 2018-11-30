4a1cb4e880c2c03bad1cc6f02eb91499071f2219
Hüseyin YİĞİT (BILGEM) <huseyin.yigit@tbtk.gov.tr>
2018-11-30 15:37:44 +0300

    old browser compability,

--- custom/authorize2.js
+++ custom/authorize2.js
@@ -1,15 +1,15 @@
 function authorize(isRequired, failureRedirect, callback){
-  let request = new XMLHttpRequest()
+  var request = new XMLHttpRequest()
   request.open("GET", "/user", true)
-  request.onload = function () {
-    user = JSON.parse(request.responseText)
+  request.onload = function (e) {
+    user = JSON.parse(e.target.responseText)
     if (!user.username) {
       if (isRequired) window.location.href = "/login?source="+failureRedirect
       else {
-        let request = new XMLHttpRequest()
+        var request = new XMLHttpRequest()
         request.open("POST", "/login", true)
         request.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-        let json = { username: "guest", password: "guest" }
+        var json = { username: "guest", password: "guest" }
         request.send(JSON.stringify(json))
         callback({username:"guest", name:"Guest"})
       }
